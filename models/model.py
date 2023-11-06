c897f6694850509477a4d931417a084bd2a0e0c9
Sunny Chan <schan6803@gmail.com>
2023-11-06 04:57:40 -0500

    initializing model stuff

---
+++ models/model.py
@@ -0,0 +1,23 @@
+import torch
+from torch import nn
+from torch.utils.data import DataLoader
+from torchvision import datasets, transforms
+import torchvision.models as models
+
+class NeuralNetwork(nn.Module):
+    def __init__(self):
+        super().__init__()
+        resnet = models.resnet50(weights="IMAGENET1K_V1")
+        self.flatten = nn.Flatten()
+        self.linear_relu_stack = nn.Sequential(
+            nn.Linear(28*28, 512),
+            nn.ReLU(),
+            nn.Linear(512, 512),
+            nn.ReLU(),
+            nn.Linear(512, 10),
+        )
+
+    def forward(self, x):
+        x = self.flatten(x)
+        logits = self.linear_relu_stack(x)
+        return logits
--- src/models/train_model.py
+++ src/models/train_model.py
@@ -1,7 +1,7 @@
-
 import torch
 from torch import nn
-from torch.utils.data import Dataloader
+from torch.utils.data import DataLoader
 from torchvision import datasets, transforms
+from torchvision.models import regnet50
 
-torch.cuda.is_available()
+device = "cuda" if torch.cuda.is_available() else "cpu"
