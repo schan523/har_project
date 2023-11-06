76885c6fca11a3500b994d01434554b6ace6ae97
schan523 <schan6803@gmail.com>
2023-11-05 19:04:50 -0500

    Uploading project template

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Training and deploying a CNN on a dataset of human activity images',
+    author='Sunny Chan',
+    license='MIT',
+)
