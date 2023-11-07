4f624db0095a9452276b39b13f2196de9d01c85d
schan523 <schan6803@gmail.com>
2023-11-06 20:17:15 -0500

    model stuff

--- src/models/train_model.py
+++ src/models/train_model.py
@@ -3,5 +3,9 @@ from torch import nn
 from torch.utils.data import DataLoader
 from torchvision import datasets, transforms
 from torchvision.models import regnet50
+from models.model import NeuralNetwork
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
+
+#Creating an instance of the model
+model = NeuralNetwork().to(device)
