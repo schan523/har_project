34ec1ae56d4da839a2af0cbcfb25bf893d7f02fa
Sunny Chan <schan6803@gmail.com>
2023-11-13 00:51:29 -0500

    updating model def

--- models/model.py
+++ models/model.py
@@ -4,20 +4,20 @@ from torch.utils.data import DataLoader
 from torchvision import datasets, transforms
 import torchvision.models as models
 
-class NeuralNetwork(nn.Module):
+class BaseClassifier(nn.Module):
     def __init__(self):
-        super().__init__()
-        resnet = models.resnet50(weights="IMAGENET1K_V1")
-        self.flatten = nn.Flatten()
-        self.linear_relu_stack = nn.Sequential(
-            nn.Linear(28*28, 512),
-            nn.ReLU(),
-            nn.Linear(512, 512),
-            nn.ReLU(),
-            nn.Linear(512, 10),
-        )
+        super().__init__(self, n_classes)
+        model = models.resnet50(weights="IMAGENET1K_V1")
+        modules = list(model.children())[:-1] #deleting fully connected layer
+        self.resnet = nn.Sequential(*modules)
+
+        #freeze pre-trained layers
+        for param in self.resnet.parameters():
+            param.requires_grad = False
+
+        self.fc = nn.Linear(model.fc.in_features, n_classes)
 
     def forward(self, x):
-        x = self.flatten(x)
-        logits = self.linear_relu_stack(x)
+        x = self.resnet(x)
+        logits = self.fc(x)
         return logits
