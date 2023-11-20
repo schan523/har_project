7fbca83d05e2e8d8dddaf6dcbe7c56ad79c7372b
schan523 <schan6803@gmail.com>
2023-11-20 15:10:04 -0500

    model stuff

--- models/model.py
+++ models/model.py
@@ -5,22 +5,6 @@ from torchvision import datasets, transforms
 import torchvision.models as models
 
 #pre-trained model adapted for our task
-class NeuralNetwork(nn.Module):
-    def __init__(self):
-        super().__init__()
-        model = models.resnet50(weights="IMAGENET1K_V1")
-        num_features = model.fc.in_features
-        self.fc = nn.Sequential(
-            nn.Linear(num_features, 15)
-        )
-    
-    def forward(self, x):
-        x = self.fc(x)
-        return x
-
-
-
-
 class BaseClassifier(nn.Module):
     def __init__(self):
         super().__init__(self, n_classes)
--- src/models/train_model.py
+++ src/models/train_model.py
@@ -1,29 +1,55 @@
 import torch
 from torch import nn
+from torchsummary import summary    
 from torch.utils.data import DataLoader
 from torchvision import datasets, transforms
 from torchvision.models import regnet50
+from torch.utils.tensorboard import SummaryWriter
 from models.model import BaseClassifier
 
-device = "cuda" if torch.cuda.is_available() else "cpu"
-
-model = BaseClassifier(15).to(device)
-optimizer = torch.optim.Adam(model.parameters())
-loss_fn = torch.nn.CrossEntropyLoss()
-
-def train():
-    for i, data in enumerate(train_dataloader):
+def train(model, train_loader, optimizer, loss_fn):
+    for i, data in enumerate(train_loader):
+        model.train()
         inputs, labels = data  #inputs = X, labels = y
 
         optimizer.zero_grad()
         outputs = model(inputs)
-        pred = outputs.argmax(-1)
+        #pred = outputs.argmax(-1)
 
         #Calculating loss
-        loss = loss_fn(pred, labels)
-        accuracy = sum(pred==labels) / len(labels)
+        loss = loss_fn(outputs, labels)
+        #accuracy = sum(pred==labels) / len(labels)
         loss.backward()
         optimizer.step()
-        
-        return loss, accuracy
+        return loss#, accuracy
+
+@torch.no_grad()
+def validate(model, train_loader, optimizer, loss_fn):
+    for i, data in enumerate(train_loader):
+        model.eval()
+        inputs, labels = data
+        outputs = model(inputs)
+        loss = loss_fn(outputs, labels)
+        #pred = outputs.argmax(-1)
+        #accuracy = sum(pred==labels) / len(labels)
+        return loss#, accuracy
+
+#Main training loop
+num_epochs = 10
+writer = SummaryWriter()
+device = "cuda" if torch.cuda.is_available() else "cpu"
+model = BaseClassifier(15).to(device)
+print(summary(model, (1, 224, 224)))
+optimizer = torch.optim.Adam(model.parameters())
+loss_fn = torch.nn.CrossEntropyLoss()
+
+for epoch in range(num_epochs):
+    for i, data in enumerate(train_loader):
+        train_loss, train_accuracy = train(model, train_loader, optimizer, loss_fn)
+        writer.add_scalar("Train Loss", train_loss, epoch)
+
+
+#Confusion matrices
+
+
 
