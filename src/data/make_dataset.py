f5e2a14848b4ed8006f8aab226c84c44d50531a8
Sunny Chan <schan6803@gmail.com>
2023-11-22 00:44:02 -0500

    adding from colab notebook

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -1,30 +1,53 @@
-# -*- coding: utf-8 -*-
-import click
-import logging
-from pathlib import Path
-from dotenv import find_dotenv, load_dotenv
+import pandas as pd
+import os
+from PIL import Image
+
+from torch.utils.data import Dataset, DataLoader
+from torchvision import transforms
+import torchvision.models as models
+import matplotlib.pyplot as plt
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+
+train_csv = pd.read_csv(".\data\Testing_set.csv")
+test_csv = pd.read_csv(".\data\Testing_set.csv")
+
+#labels to numbers
+label_names = {'sitting':0,'using_laptop':1, 'hugging':2, 'sleeping':3,'drinking':4,'clapping':5, 'dancing':6, 'cycling':7,'calling':8,'laughing':9, 'eating':10,'fighting':11,'listening_to_music':12,'running':13,'texting':14}
+
+# Custom image Dataset
+class ImageDataSet(Dataset):
+    def __init__(self, csv_file, root_dir, transform=None):
+        self.root_dir = root_dir
+
+        self.df = pd.read_csv(csv_file)
+        self.transform = transforms.Compose([
+            transforms.Resize([224, 224]),
+            transforms.ToTensor(),
+            # std multiply by 255 to convert img of [0, 255]
+            # to img of [0, 1]
+            transforms.Normalize((0.485, 0.456, 0.406),
+                                 (0.229*255, 0.224*255, 0.225*255))]
+        )
+        self.img_name = self.df['filename']
+        self.transform = transforms
+        #test doesnt have column
 
 
-@click.command()
-@click.argument('input_filepath', type=click.Path(exists=True))
-@click.argument('output_filepath', type=click.Path())
-def main(input_filepath, output_filepath):
-    """ Runs data processing scripts to turn raw data from (../raw) into
-        cleaned data ready to be analyzed (saved in ../processed).
-    """
-    logger = logging.getLogger(__name__)
-    logger.info('making final data set from raw data')
+    def __len__(self):
+        return self.df.shape[0] #self.y.shape[0]?
+
+    def __getitem__(self, idx):
+        img = Image.open(os.path.join(self.root_dir, self.img_name[idx]))
+
+        if self.transform is not None:
+          img = self.transform(img)
+
+        #label = torch.tensor(self.df['label'][idx], dtype=torch.long)  # Convert label to a PyTorch tensor
+        label = self.df['label'][idx]
+        label = label_to_number.get(label, -1)
+        return img, label
 
 
-if __name__ == '__main__':
-    log_fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
-    logging.basicConfig(level=logging.INFO, format=log_fmt)
-
-    # not used in this stub but often useful for finding various files
-    project_dir = Path(__file__).resolve().parents[2]
-
-    # find .env automagically by walking up directories until it's found, then
-    # load up the .env entries as environment variables
-    load_dotenv(find_dotenv())
-
-    main()
--- src/models/train_model.py
+++ src/models/train_model.py
@@ -6,8 +6,46 @@ from torchvision import datasets, transforms
 from torchvision.models import resnet50
 from torch.utils.tensorboard import SummaryWriter
 from datetime import datetime
+from data.make_dataset import ImageDataSet
+from sklearn.model_selection import train_test_split
 from .model import BaseClassifier
 
+#data preprocessing
+# Data augmentation for training set
+train_transform = transforms.Compose([
+    transforms.RandomResizedCrop(224),
+    transforms.RandomHorizontalFlip(),
+    transforms.RandomRotation(10),
+    transforms.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2, hue=0.2),
+    transforms.ToTensor(),
+])
+
+# No data augmentation for testing set
+test_transform = transforms.Compose([
+    transforms.Resize(256),
+    transforms.CenterCrop(224),
+    transforms.ToTensor(),
+])
+
+# DataSet and DataLoader
+train_val_dataset = ImageDataSet(
+    csv_file="/content/drive/MyDrive/Human Action Recognition/Training_set.csv",
+    root_dir="/content/drive/MyDrive/Human Action Recognition/train/",
+    transform=train_transform
+)
+
+#Split into train and validation sets
+train_dataset, val_dataset = train_test_split(train_val_dataset, test_size=0.18)
+
+train_loader = DataLoader(
+    dataset=train_dataset,
+    batch_size=32,
+    drop_last=True,
+    shuffle=True,
+    num_workers=0
+)
+
+val_loader = DataLoader(dataset=val_dataset, batch_size=32, drop_last=True, shuffle=True, num_workers=0)
 #pre-trained model adapted for our task
 class BaseClassifier(nn.Module):
     def __init__(self):
